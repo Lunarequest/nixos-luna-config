8d4edc27651932e14bf164e99dc793038832faae
Luna D Dragon <luna@nullrequest.com>
2024-05-19 19:23:02 +0530

    get kernel building with clang and thin lto

--- hosts/lifrith/configuration.nix
+++ hosts/lifrith/configuration.nix
@@ -9,16 +9,41 @@
 }: {
   nixpkgs.overlays = [
     (self: super: {
-      luna_linux = let
-        baseKernel = pkgs.linuxPackages_latest;
-      in
-        pkgs.linuxManualConfig {
-          inherit (baseKernel) src modDirVersion;
-          stdenv = pkgs.llvmPackages_18.stdenv;
-          version = "${baseKernel.kernel.version}luna";
-          configfile = ../../kernel/.config;
-          allowImportFromDerivation = true;
-        };
+      luna_linux =
+        (let
+          baseKernel = pkgs.linuxPackages_latest;
+        in
+          pkgs.linuxManualConfig {
+            inherit (baseKernel.kernel) src modDirVersion;
+            stdenv = pkgs.llvmPackages_18.stdenv;
+            version = "${baseKernel.kernel.version}-luna";
+            configfile = ../../kernel/.config;
+            allowImportFromDerivation = true;
+          })
+        .overrideAttrs (old: {
+          nativeBuildInputs = with pkgs;
+            old.nativeBuildInputs
+            ++ [
+              llvmPackages_18.lld
+              llvmPackages_18.clangUseLLVM
+              llvmPackages_18.llvm
+            ];
+          LLVM = "1";
+          LLVM_IAS = "1";
+          CC = "${pkgs.llvmPackages_18.clangUseLLVM}/bin/clang";
+          HOSTCC = "${pkgs.llvmPackages_18.clangUseLLVM}/bin/clang";
+          LD = "${pkgs.llvmPackages_18.lld}/bin/ld.lld";
+          HOSTLD = "${pkgs.llvmPackages_18.lld}/bin/ld.lld";
+          AR = "${pkgs.llvmPackages_18.llvm}/bin/llvm-ar";
+          HOSTAR = "${pkgs.llvmPackages_18.llvm}/bin/llvm-ar";
+          NM = "${pkgs.llvmPackages_18.llvm}/bin/llvm-nm";
+          STRIP = "${pkgs.llvmPackages_18.llvm}/bin/llvm-strip";
+          OBJCOPY = "${pkgs.llvmPackages_18.llvm}/bin/llvm-objcopy";
+          OBJDUMP = "${pkgs.llvmPackages_18.llvm}/bin/llvm-objdump";
+          READELF = "${pkgs.llvmPackages_18.llvm}/bin/llvm-readelf";
+          HOSTCXX = "${pkgs.llvmPackages_18.clangUseLLVM}/bin/clang++";
+          hardeningDisable = ["all"];
+        });
     })
   ];
   imports = [
--- hosts/lifrith/modules/kernel.nix
+++ hosts/lifrith/modules/kernel.nix
@@ -5,29 +5,5 @@
   ...
 }: {
   boot.kernelParams = ["zswap.enabled=1"];
-  boot.kernelPackages = pkgs.luna_linux.extend (self: super: {
-    kernel = super.kernel.overrideAttrs (old: {
-      nativeBuildInputs = with pkgs;
-        old.nativeBuildInputs
-        ++ [
-          llvmPackages_18.lld
-          llvmPackages_18.clangUseLLVM
-          pkgs.llvmPackages_18.llvm
-        ];
-      LLVM = "1";
-      LLVM_IAS = "1";
-      CC = "${pkgs.llvmPackages_18.clangUseLLVM}/bin/clang";
-      HOSTCC = "${pkgs.llvmPackages_18.clangUseLLVM}/bin/clang";
-      LD = "${pkgs.llvmPackages_18.lld}/bin/ld.lld";
-      HOSTLD = "${pkgs.llvmPackages_18.lld}/bin/ld.lld";
-      AR = "${pkgs.llvmPackages_18.llvm}/bin/llvm-ar";
-      HOSTAR = "${pkgs.llvmPackages_18.llvm}/bin/llvm-ar";
-      NM = "${pkgs.llvmPackages_18.llvm}/bin/llvm-nm";
-      STRIP = "${pkgs.llvmPackages_18.llvm}/bin/llvm-strip";
-      OBJCOPY = "${pkgs.llvmPackages_18.llvm}/bin/llvm-objcopy";
-      OBJDUMP = "${pkgs.llvmPackages_18.llvm}/bin/llvm-objdump";
-      READELF = "${pkgs.llvmPackages_18.llvm}/bin/llvm-readelf";
-      HOSTCXX = "${pkgs.llvmPackages_18.clangUseLLVM}/bin/clang++";
-    });
-  });
+  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.luna_linux;
 }
