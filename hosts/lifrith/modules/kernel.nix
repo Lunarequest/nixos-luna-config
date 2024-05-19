95fa36386186a37c18cb4f8a19567e471266151f
Luna D Dragon <luna@nullrequest.com>
2024-05-19 20:19:29 +0530

    add patches for to kernel

--- hosts/lifrith/modules/kernel.nix
+++ hosts/lifrith/modules/kernel.nix
@@ -5,5 +5,27 @@
   ...
 }: {
   boot.kernelParams = ["zswap.enabled=1"];
+  boot.kernelPatches = [
+    {
+      name="tcp-bbrv3";
+      patch = ../../../kernel/patches/0001-bbr3.patch;
+    }
+    {
+      name="misc-fixes";
+      patch=../../../kernel/patches/0002-fixes.patch;
+    }
+    {
+      name="ntsync";
+      patch=../../../kernel/patches/0003-ntsync.patch;
+      extraStructuredConfig = {
+        NTSYNC = lib.kernel.mkForce lib.kernel.module;
+      };
+    }
+    {
+      name = "upgrade-zstd";
+      patch = ../../../kernel/patches/0004-zstd.patch;
+    }
+  ];
   boot.kernelPackages = pkgs.linuxPackagesFor pkgs.luna_linux;
 }
+
